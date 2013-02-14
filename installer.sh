0a3e94eba5a0320aaf51745810b9f6c8efcb04e3
Lukasz Gruner <ukaszg@gmail.com>
2013-02-14 22:13:49 +0100

    create zfs config script

---
+++ installer.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+set -e
+### Config #########################
+POOLNAME="sys"
+BENAME="stable9"
+BRANCH="stable/9"
+SWAPSIZE="4G"
+DISK="$1"
+
+### Check params ###################
+[ -c "$DISK" ] || {
+	echo "usage: $0 /dev/(disk|slice)";
+	echo "	$0 /dev/ada0s4a";
+	exit 1;
+}
+
+### Avaliable options ##############
+RO="-o readonly=on"
+ATIME="-o atime=on"
+UTF8ONLY="-o utf8only=on"
+COMPR="-o compression=on"
+NOCOMPR="-o compression=off"
+NOMNT="-o mountpoint=none"
+LEGACYMNT="-o mountpoint=legacy"
+NOSUID="-o setuid=off"
+DEV="-o devices=on"
+NODEV="-o devices=off"
+EXEC="-o exec=on"
+NOEXEC="-o exec=off"
+
+### Pool ###########################
+echo "Creating zpool $POOLNAME on $DISK"
+zpool create -m none $POOLNAME $DISK
+zfs set mountpoint=none $POOLNAME
+zfs set checksum=fletcher4 $POOLNAME
+zfs set atime=off $POOLNAME
+
+### Datasets #######################
+echo "Creating datasets"
+R="$POOLNAME/ROOT/$BENAME"  # boot enviroment
+L="$POOLNAME/LOCAL"         # local/shared datasets
+#          _OPTIONS_______________________ _PATH_________________
+zfs create $NOMNT $UTF8ONLY $NODEV         $POOLNAME/ROOT
+zfs create $NOMNT                          $R
+zfs create $DEV                            $R/dev
+zfs create $COMPR   $EXEC   $NOSUID        $R/tmp
+zfs create          $EXEC   $NOSUID        $R/compat
+zfs create $COMPR   $EXEC   $NOSUID        $R/etc
+
+zfs create                                 $R/usr
+zfs create $COMPR   $NOEXEC $NOSUID        $R/usr/src
+zfs create                  $NOSUID        $R/usr/obj
+zfs create $COMPR           $NOSUID        $R/usr/ports
+zfs create $NOCOMPR $NOEXEC                $R/usr/ports/distfiles
+zfs create $NOCOMPR $NOEXEC                $R/usr/ports/packages
+zfs create                                 $R/usr/local
+zfs create $COMPR           $NOSUID        $R/usr/local/etc
+
+zfs create                                 $R/var
+zfs create          $NOEXEC $NOSUID        $R/var/db
+zfs create $COMPR                          $R/var/db/ports
+zfs create                                 $R/var/db/portsnap
+zfs create $COMPR   $EXEC                  $R/var/db/pkg
+zfs create $COMPR   $NOEXEC $NOSUID        $R/var/crash
+zfs create          $NOEXEC $NOSUID $RO    $R/var/empty
+zfs create $COMPR   $NOEXEC $NOSUID        $R/var/log
+zfs create $COMPR   $NOEXEC $NOSUID $ATIME $R/var/mail
+zfs create          $NOEXEC $NOSUID        $R/var/run
+zfs create $COMPR   $EXEC   $NOSUID        $R/var/tmp
+
+zfs create $NOMNT $UTF8ONLY $NOSUID $NODEV $L
+zfs create                                 $L/home
+zfs create $COMPR   $NOEXEC                $L/home/pub
+zfs create -V $SWAPSIZE \
+              -o org.freebsd:swap=on \
+              -o checksum=off \
+              -o sync=disabled \
+              -o primarycache=none \
+              -o secondarycache=none       $L/swap
+
+zpool set bootfs=$R $POOLNAME
+### Result #########################
+echo
+echo "$0 finished, results:"
+zfs list -t all -o name,compression,exec,setuid,readonly,checksum,utf8only,atime,devices,sync
